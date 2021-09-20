290a30b8dce8a72cd8427383938e4fd6830e24a9
Zuazo99 <gzuazo63@gmail.com>
2021-09-20 12:06:53 +0200

    Funtzionalitate berria programatzen...lehenengo bertsioa.

---
+++ src/main/java/com/isad/bertsioak/Funtzionalitate.java
@@ -0,0 +1,13 @@
+package com.isad.bertsioak;
+
+public class Funtzionalitate {
+
+
+
+public static void main(String args[]){
+System.out.println("Hemen kode konplexu bat izan beharko genuke :-) ");
+}
+
+
+
+}
